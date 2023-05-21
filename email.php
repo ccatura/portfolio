497f4ec7b9821623ed83f6174e48e608715334d7
ccatura <charlie@charliecatura.com>
2023-05-20 21:16:15 -0400

    tweaking email settings

--- email.php
+++ email.php
@@ -7,16 +7,16 @@
     $message = "New message from your portfolio page from <strong>" . $name . "</strong> (" . $email . ")<br><hr>";
     $message .= $_POST['message'];
 
-    $header = "From: " . $name . " <" . $email . "> \r\n";
+    // $header = "From: " . $name . " <" . $email . "> \r\n";
     //  $header .= "Cc:afgh@somedomain.com \r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n";
 
-    $sendmail = mail ($to, $subject, $message, $header);
+    $sendmail = mail ($to, $subject, $message, $header,  '-f ' . $email . ' -F "' . $name . '"');
 
     if( $sendmail == true ) {
         echo "<h1>Your message was sent successfully!</h1><h3><a href='./'>Click here to go back</a></h3>";
-        echo $message;
+        echo $message . "<br><br>";
         echo $header;
     } else {
         echo "<h1>Message could not be sent.</h1><h3><a href='./'>Click here to try again</a></h3>";

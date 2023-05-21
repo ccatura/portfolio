4ba90e871807812794ff6557721cefb07dff4a22
ccatura <charlie@charliecatura.com>
2023-05-20 21:35:52 -0400

    tweaking email settings

--- email.php
+++ email.php
@@ -15,11 +15,11 @@
     $sendmail = mail($to, $subject, $message, implode("\r\n", $header));
 
     if( $sendmail == true ) {
-        echo "<h1>Your message was sent successfully!</h1><h3><a href='./'>Click here to go back</a></h3>";
+        echo "<h1>Your message was sent successfully!</h1><h3><a href='javascript:close_window();'>Click here to close this tab.</a></h3>";
         echo $message . "<br><br>";
         echo implode("\r\n", $header);
     } else {
-        echo "<h1>Message could not be sent.</h1><h3><a href='./'>Click here to try again</a></h3>";
+        echo "<h1>Message could not be sent.</h1><h3><a href='javascript:close_window();'>Click here to try again.</a></h3>";
     }
 ?>
 

24d0b26eb6ddfa2bb5a23e5c42ff9e2d92396be6
ccatura <charlie@charliecatura.com>
2023-06-23 11:06:53 -0400

    worked on spam

--- email.php
+++ email.php
@@ -1,30 +1,34 @@
 <?php
     $to = "ccatura@gmail.com";
-    $subject = $_POST['subject'];
     $email = $_POST['email'];
     $name = $_POST['name'];
     $good_to_go = $_POST['good-to-go'];
-
-    $message = "Good to go? " . $good_to_go . "<br>";
-    $message .= "New message from your portfolio page from <strong>" . $name . "</strong> (" . $email . ")<br><hr>";
-    $message .= $_POST['message'];
 
     $header[] = "From: $email";
     $header[] = "MIME-Version: 1.0";
     $header[] = "Content-type: text/html";
 
     if($good_to_go == 'true') {
-        $sendmail = mail($to, $subject, $message, implode("\r\n", $header));
-    
-        if( $sendmail == true ) {
-            echo "<h1>Your message was sent successfully!</h1><h3><a href='#' onclick='window.close();return false;'>Click here to close this tab.</a></h3>";
-            // echo $message . "<br><br>";
-            // echo implode("\r\n", $header);
-        } else {
-            echo "<h1>Message could not be sent.</h1><h3><a href='#' onclick='window.close();return false;'>Click here to try again.</a></h3>";
-        }
+        $subject = $_POST['subject'];
+        $message  = "Good to go? " . $good_to_go . "<br>";
+        $message .= "New message from your portfolio page from <strong>" . $name . "</strong> (" . $email . ")<br><hr>";
+        $message .= $_POST['message'];
     } else {
-        echo "<h1>There was an error. Go back and try again.</h1><h3><a href='#' onclick='window.close();return false;'>Click here to close this tab.</a></h3>";
+        $subject = "Spam from my portfolio webpage";
+        $message  = "Stopped spammer:<br>";
+        $message .= "Name: " . $name . "<br>";
+        $message .= "Email: " . $email . "<br>";
+        $message .= "Message: " . $_POST['message'] . "<br>";
+    }
+
+    $sendmail = mail($to, $subject, $message, implode("\r\n", $header));
+
+    if( $sendmail == true ) {
+        echo "<h1>Your message was sent successfully!</h1><h3><a href='#' onclick='window.close();return false;'>Click here to close this tab.</a></h3>";
+        // echo $message . "<br><br>";
+        // echo implode("\r\n", $header);
+    } else {
+        echo "<h1>Message could not be sent.</h1><h3><a href='#' onclick='window.close();return false;'>Click here to try again.</a></h3>";
     }
 ?>
 
